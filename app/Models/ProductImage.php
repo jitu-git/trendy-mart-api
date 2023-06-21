0e3aaa06e9a09c179061d67015b543747de6f602
Jitendra <jitendrameena38@gmail.com>
2023-06-21 11:36:13 +0530

    set image path for front end

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -6,4 +6,11 @@ namespace App\Models;
 class ProductImage extends AppModel
 {
     protected $fillable = ['product_id', 'image'];
+
+    public function getImageAttribute($value) {
+        if(is_admin()){
+            return $value;
+        }
+        return $value ? asset("storage/{$value}") : null; 
+    }
 }
